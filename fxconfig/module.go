fb8cdeb5c4476ccf59b56317b4066fab9868b840
templatedop <07raghuram@gmail.com>
2024-08-23 11:03:47 +0530

    first commit

---
+++ fxconfig/module.go
@@ -0,0 +1,34 @@
+package fxconfig
+
+import (
+	"github.com/templatedop/fxconfigs/config"
+	"go.uber.org/fx"
+)
+
+
+const ModuleName = "config"
+
+var FxConfigModule = fx.Module(
+	ModuleName,
+	fx.Provide(
+		config.NewDefaultConfigFactory,
+		NewFxConfig,
+	),
+)
+
+// FxConfigParam allows injection of the required dependencies in [NewFxConfig].
+type FxConfigParam struct {
+	fx.In
+	Factory config.ConfigFactory
+}
+
+// NewFxConfig returns a [config.Config].
+func NewFxConfig(p FxConfigParam) (*config.Config, error) {
+	return p.Factory.Create(
+		config.WithFileName("config"),
+		config.WithFilePaths(
+			".",
+			"./configs",
+		),
+	)
+}
